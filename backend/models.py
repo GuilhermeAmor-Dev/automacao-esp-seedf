f707e6d667b234349ca9b2c49b76297a94a234d9
Marcos Santos <marcos.adriano@sempreceub.com>
2025-09-06 16:36:56 -0300

    Difinição das tabelas no Banco de Dados

---
+++ backend/models.py
@@ -0,0 +1,13 @@
+# Esse arquivo define como é as tabelas no banco de dados
+from sqlalchemy import Column, Integer, String, DateTime, func
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(20), nullable=False)
+    role = Column(String(20), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
