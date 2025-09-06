cfa6c5bcdda21c91d4d4e583c2ccfb6981c8df71
Marcos Santos <marcos.adriano@sempreceub.com>
2025-09-06 18:41:39 -0300

    Ajuste no Model para Timestamps

--- backend/models.py
+++ backend/models.py
@@ -6,8 +6,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, nullable=False)
-    password = Column(String(20), nullable=False)
+    username = Column(String(50), unique=True, nullable=False, index=True)
+    password = Column(String(200), nullable=False)
     role = Column(String(20), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(),onupdate=func.now())
