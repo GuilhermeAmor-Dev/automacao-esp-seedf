1ad8614bc4202a62ee5be3eb0ff492fc6c17626f
Marcos Santos <marcos.adriano@sempreceub.com>
2025-09-06 18:39:05 -0300

    Troca de retorno do erro para HTTPException

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 # Esse arquivo é o ponto de entrada da aplicação FastAPI e rota POST para criar usuários
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from . import models, schemas, crud, database
 
 # Cria as tabelas no banco
@@ -18,7 +19,14 @@ def get_db():
 
 @app.post("/users/", response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_username(db, username=user.username)
-    if db_user:
-        return {"error": "Usuário já existe"}
-    return crud.create_user(db=db, user=user)
+    # checagem prévia
+    if crud.get_user_by_username(db, username=user.username):
+        # antes: return {"error": "Usuário já existe"}  -> causava 500 - espera-se que esse erro seja corrigido.
+        raise HTTPException(status_code=400, detail="Usuário já existe")
+
+    try:
+        return crud.create_user(db=db, user=user)
+    except IntegrityError:
+        # se por acaso ocorrer corrida de dados / constraint UNIQUE
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Usuário já existe")
